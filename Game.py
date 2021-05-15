5d7b6909fc4b1f42cba3aabfa391e3bfca3b2c7b
Sevangel <57137525+Sevangel@users.noreply.github.com>
2021-05-15 13:31:58 +0200

    Sélection des personnages pour le joueur 1 et 2

--- Game.py
+++ Game.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame.freetype
 import sys
 from Menu import *
 from Données import *
@@ -6,6 +7,70 @@ from Données import *
 class Game() :
     def __init__(self) :
         self.run = True
-        self.playgame = False
         self.titre = pygame.display.set_caption("Insults Battle")
-                    
+        self.screen = pygame.display.set_mode((LARGEUR,HAUTEUR))
+        self.x = 50
+        self.y = 150
+        self.ybutton = 100
+
+    def gameselectJ1(self) :
+        
+        self.screen.fill((255,255,255))
+
+        titlefont = pygame.freetype.Font('Ressources/8bitwonder.ttf',30)
+        textfont = pygame.freetype.Font('Ressources/8bitwonder.ttf', 20)
+
+        Joueur1, rect = titlefont.render("Joueur 1", (0,0,0))
+        title, rect = titlefont.render("Selectionnez votre personnage", (0,0,0))
+        perso1, rect = textfont.render("Flynn", (0,0,0))
+        perso2, rect = textfont.render("Abuelita", (0,0,0))
+        perso3, rect = textfont.render("Shrek", (0,0,0))
+
+        self.screen.blit(Joueur1, (self.x, self.y - 80))
+        self.screen.blit(title, (self.x,self.y))
+        self.screen.blit(perso1, (self.x + 100, self.y + 100))
+        self.screen.blit(perso2, (self.x + 100, self.y + 180))
+        self.screen.blit(perso3, (self.x + 100, self.y + 260))
+
+    def gameselectJ2(self) :
+        
+        self.screen.fill((255,255,255))
+
+        titlefont = pygame.freetype.Font('Ressources/8bitwonder.ttf',30)
+        textfont = pygame.freetype.Font('Ressources/8bitwonder.ttf', 20)
+
+        Joueur1, rect = titlefont.render("Joueur 2", (0,0,0))
+        title, rect = titlefont.render("Selectionnez votre personnage", (0,0,0))
+        perso1, rect = textfont.render("Flynn", (0,0,0))
+        perso2, rect = textfont.render("Abuelita", (0,0,0))
+        perso3, rect = textfont.render("Shrek", (0,0,0))
+
+        self.screen.blit(Joueur1, (self.x, self.y - 80))
+        self.screen.blit(title, (self.x,self.y))
+        self.screen.blit(perso1, (self.x + 100, self.y + 100))
+        self.screen.blit(perso2, (self.x + 100, self.y + 180))
+        self.screen.blit(perso3, (self.x + 100, self.y + 260))
+
+    def gameselectbutton(self) :
+        textfont = pygame.freetype.Font('Ressources/8bitwonder.ttf', 20)
+        button, rect = textfont.render("X", (0,0,0))
+        self.screen.blit(button, (self.x, self.y + self.ybutton))
+
+    def gameselectmoveup(self) :
+        if self.ybutton <= 130 :
+            self.ybutton = self.ybutton - 0
+        else :
+            self.ybutton = self.ybutton - 80
+
+    def gameselectmovedown(self) :
+        if self.ybutton >= 250 :
+            self.ybutton = self.ybutton + 0
+        else :
+            self.ybutton = self.ybutton + 80
+
+    def gameplay(self) :
+        self.screen = pygame.display.set_mode((LARGEUR,HAUTEUR))
+        background = pygame.image.load("Ressources/Paysage.jpeg")
+        background = pygame.transform.scale(background,(LARGEUR, HAUTEUR))
+
+        self.screen.blit(background,(0,0))
--- projet.py
+++ projet.py
@@ -21,18 +21,109 @@ menu = Menu()
 #Etat de l'écran
 
 ecran = "FirstPage"
+PersoJ1 = ""
+PersoJ2 =""
+
 
 #Boucle du jeu
 
 while True :
     
     if ecran == "FirstPage" :
-
         menu.menuscreen()
         menu.menubutton()
 
     elif ecran == "APropos" :
         menu.Apropos()
+
+    elif ecran == "SelectionJ1" :
+
+
+        game.gameselectJ1()
+        game.gameselectbutton()
+
+        for event in pygame.event.get() :
+            if event.type == pygame.QUIT :
+                pygame.quit()
+                sys.quit()
+
+            elif event.type == pygame.KEYDOWN :
+
+                if event.key == pygame.K_DOWN :
+                    game.gameselectmovedown()
+ 
+                elif event.key == pygame.K_UP :
+                    game.gameselectmoveup()
+
+                elif event.key == pygame.K_ESCAPE and ecran == "SelectionJ1" :
+                    ecran = "FirstPage"
+                    
+                elif event.key == pygame.K_RETURN and ecran == "SelectionJ1" :
+                    if game.ybutton == 100 :
+                        PersoJ1 = "Flynn"
+                        ecran = "SelectionJ2"
+                
+                    elif game.ybutton == 180 :
+                        PersoJ1 = "Abuelita"
+                        ecran = "SelectionJ2"
+
+                    elif game.ybutton == 260 :
+                        PersoJ1 = "Shrek"
+                        ecran = "SelectionJ2"
+
+    elif ecran == "SelectionJ2" :
+
+        game.gameselectJ2()
+        game.gameselectbutton()
+
+        for event in pygame.event.get() :
+
+            if event.type == pygame.QUIT :
+                pygame.quit()
+                sys.quit()
+
+            elif event.type == pygame.KEYDOWN :
+
+                if event.key == pygame.K_DOWN :
+                    game.gameselectmovedown()
+ 
+                elif event.key == pygame.K_UP :
+                    game.gameselectmoveup()
+
+                elif event.key == pygame.K_ESCAPE and ecran == "SelectionJ1" :
+                    ecran = "FirstPage"
+                    
+                elif event.key == pygame.K_RETURN and ecran == "SelectionJ2" :
+                    if game.ybutton == 100 :
+                        PersoJ2 = "Flynn"
+                        print(PersoJ1)
+                        print(PersoJ2) 
+                        ecran = "Jeu"
+                            
+                    elif game.ybutton == 180 :
+                        PersoJ2 = "Abuelita"
+                        print(PersoJ1)
+                        print(PersoJ2) 
+                        ecran = "Jeu"
+
+                    elif game.ybutton == 260 :
+                        PersoJ2 = "Shrek"
+                        print(PersoJ1)
+                        print(PersoJ2) 
+                        ecran = "Jeu"
+
+                elif event.key == pygame.K_ESCAPE and ecran == "SelectionJ2" :
+                    ecran = "SelectionJ1"
+
+    elif ecran == "Jeu" :
+        game.gameplay()
+
+        for event in pygame.event.get() :
+
+            if event.type == pygame.QUIT :
+                pygame.quit()
+                sys.quit()
+
 
     for event in pygame.event.get() :
 
@@ -43,7 +134,6 @@ while True :
         elif event.type == pygame.KEYDOWN :
 
             if event.key == pygame.K_DOWN :
-
                 menu.buttonmovedown()
  
             elif event.key == pygame.K_UP :
@@ -51,17 +141,18 @@ while True :
 
             elif event.key == pygame.K_RETURN :
                 if menu.ybutton == 100 :
-                    game.playgame = True
+                    ecran = "SelectionJ1"
                 
-                if menu.ybutton == 180 :
+                elif menu.ybutton == 180 :
                     ecran = "APropos"
 
-                if menu.ybutton == 260 :
+                elif menu.ybutton == 260 :
                     pygame.quit()
                     sys.quit()
 
-            elif event.key == pygame.K_ESCAPE :
+            elif event.key == pygame.K_ESCAPE and ecran == "APropos":
                 ecran = "FirstPage"
+
 
     pygame.display.update()
     frame_per_second.tick(FPS)
